2db1d1f4bc219ed747bace42f13688569ac97891
Alan Hurtarte <alan@lionmane.io>
2017-02-20 16:08:31 -0600

    Agregado sonido y botón para hacer mute

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         collection.dataSource = self
         parsePokemonCSV()
+        initAudio()
+    }
+    
+    func initAudio(){
+    
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+        
+        do{
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
     }
     
     func parsePokemonCSV(){
@@ -75,5 +94,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
     
+    @IBAction func musicBtnPress(_ sender: UIButton) {
+        
+        if musicPlayer.isPlaying{
+            musicPlayer.pause()
+            sender.alpha = 0.2
+        }else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+    }
 }
 
